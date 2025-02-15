cc027e2f0235cc5c856e3df1afb4586035adb563
Riyad Morshed Shoeb <rmshoebne14@gmail.com>
2025-02-15 15:38:02 +0600

    test number

--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -1,36 +1,27 @@
-import { ChangeDetectorRef, Component, NgZone, signal } from '@angular/core';
-import { DxHtmlEditorModule } from 'devextreme-angular';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DxHtmlEditorModule, DxNumberBoxModule } from 'devextreme-angular';
 
 @Component({
     selector: 'app-signal',
-    standalone: true,
     imports: [
-        DxHtmlEditorModule
+        DxHtmlEditorModule,
+        DxNumberBoxModule ,
+        FormsModule
     ],
     templateUrl: './signal.component.html',
     styleUrl: './signal.component.css'
 })
 export class SignalComponent {
     comment = signal("");
+    target = signal<number>(0);
+    tolerance = signal<number>(0);
+    minimum = computed(() => {
+        return this.target() - this.tolerance();
+    });
+    maximum = computed(() => {
+        return this.target() + this.tolerance();
+    });
     
-    constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
-
-    commentChanged(newValue: string) {
-        this.ngZone.runOutsideAngular(() => {
-            this.comment.set(newValue);
-        });
-    }
-
-    detach() {
-        this.cdr.detach();
-    }
-
-    attach() {
-        this.cdr.reattach();
-    }
-
-    keyDownOccurred(event: KeyboardEvent) {
-        event.stopImmediatePropagation();
-        event.stopPropagation();
-    }
+    constructor() {}
 }
